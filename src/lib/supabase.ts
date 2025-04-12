513e2c8dcc969bd11af71391fe574e48dc0c4409
Nafixhutao <135522402+Nafixhutao@users.noreply.github.com>
2025-04-12 21:48:44 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fgmdafrxsukviljhzcyi.supabase.co'
-const supabaseAnonKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnbWRhZnJ4c3Vrdmlsamh6Y3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ0NjU1NjIsImV4cCI6MjA2MDA0MTU2Mn0.leRWbeaWInHHDOq1FTQgej30lZFk8HuZDqZbiacpMDg`
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
